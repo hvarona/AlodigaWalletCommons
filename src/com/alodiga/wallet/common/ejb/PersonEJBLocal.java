e620a9ab0a5d21c6ffaee599bd9b54df9ef5427a
Unknown <lgutierrez@alodiga.com>
2020-07-22 19:29:21 -0400

    clases nueva de person

---
+++ src/com/alodiga/wallet/common/ejb/PersonEJBLocal.java
@@ -0,0 +1,33 @@
+package com.alodiga.wallet.common.ejb;
+
+import java.util.List;
+import com.alodiga.wallet.common.exception.EmptyListException;
+import com.alodiga.wallet.common.exception.GeneralException;
+import com.alodiga.wallet.common.exception.NullParameterException;
+import com.alodiga.wallet.common.exception.RegisterNotFoundException;
+import com.alodiga.wallet.common.genericEJB.EJBRequest;
+import com.alodiga.wallet.common.genericEJB.WalletGenericEJB;
+import com.alodiga.wallet.common.model.DocumentsPersonType;
+import com.alodiga.wallet.common.model.PersonType;
+import javax.ejb.Local;
+
+@SuppressWarnings(value = {"all"})
+@Local
+public interface PersonEJBLocal extends WalletGenericEJB {
+
+    //DocumentPersonType
+    public List<DocumentsPersonType> getDocumentsPersonType(EJBRequest request) throws EmptyListException, GeneralException, NullParameterException;
+
+    public DocumentsPersonType loadDocumentsPersonType(EJBRequest request) throws RegisterNotFoundException, NullParameterException, GeneralException;
+
+    public DocumentsPersonType saveDocumentsPersonType(DocumentsPersonType documentsPersonType) throws RegisterNotFoundException, NullParameterException, GeneralException;
+
+    //Tabla de PersonType
+    public List<PersonType> getPersonTypes(EJBRequest request) throws EmptyListException, GeneralException, NullParameterException;
+    
+    public List<PersonType> getPersonTypeByCountry(EJBRequest request) throws EmptyListException, GeneralException, NullParameterException;
+
+    public PersonType loadPersonType(EJBRequest request) throws RegisterNotFoundException, NullParameterException, GeneralException;
+
+    public PersonType savePersonType(PersonType personType) throws RegisterNotFoundException, NullParameterException, GeneralException;
+}
