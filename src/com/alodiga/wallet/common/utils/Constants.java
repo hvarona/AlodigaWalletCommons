c7f227f74cc4fffcb78d0dce35181e4edcc9760e
Unknown <lgutierrez@alodiga.com>
2020-08-03 09:59:10 -0400

    constantes nuevas

--- src/com/alodiga/wallet/common/utils/Constants.java
+++ src/com/alodiga/wallet/common/utils/Constants.java
@@ -254,4 +254,5 @@ public class Constants {
     public static final String STATUS_TRANSACTIONS_CODE = "PEND";
     public static final String STATUS_TRANSACTIONS_APPR = "APPR";
     public static final String STATUS_TRANSACTIONS_REJE = "REJE";
+    public static final String REQUEST_NUMBER_MANUAL_WITHDRAWAL = "MW";
 }
