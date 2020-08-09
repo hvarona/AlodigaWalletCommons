eb7663b406187dfb8c3b934e36c7f9ed1e0d603c
Jesús Gómez <jedugomvil@hotmail.com>
2020-08-09 14:32:04 -0400

    Agregadas nuevas clases para representante legal y dirección

--- src/META-INF/sql/Cambios_BD_WalletAdmin_Sprint4.sql
+++ src/META-INF/sql/Cambios_BD_WalletAdmin_Sprint4.sql
@@ -438,6 +438,79 @@ ALTER TABLE `alodigaWallet`.`account_bank`
 DROP INDEX `UnifiedRegistryId` ,
 ADD INDEX `UnifiedRegistryId` (`UnifiedRegistryId` ASC);
 
+-- cambios relacionados con el representante legal del negocio y la dirección del solicitante
+-- author: Jesús Gómez
+-- Fecha: 09/08/2020
+CREATE TABLE IF NOT EXISTS `alodigaWallet`.`person_has_address` (
+  `id` BIGINT UNIQUE NOT NULL AUTO_INCREMENT,
+  `personId` BIGINT UNIQUE NOT NULL,
+  `addressId` BIGINT(10) NOT NULL,
+  `createDate` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `updateDate` TIMESTAMP NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_person_has_address_person2_idx` (`personId` ASC),
+  INDEX `fk_person_has_address_address2_idx` (`addressId` ASC),
+  CONSTRAINT `fk_person_has_address_person2`
+    FOREIGN KEY (`personId`)
+    REFERENCES `alodigaWallet`.`person` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_person_has_address_address2`
+    FOREIGN KEY (`addressId`)
+    REFERENCES `alodigaWallet`.`address` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `alodigaWallet`.`legal_representative` (
+  `id` BIGINT UNIQUE NOT NULL AUTO_INCREMENT,
+  `personId` BIGINT UNIQUE NOT NULL,
+  `documentsPersonTypeId` INT NOT NULL,
+  `identificationNumber` VARCHAR(40) NOT NULL,
+  `identificationNumberOld` VARCHAR(40) NULL,
+  `dueDateDocumentIdentification` DATE NULL,
+  `firstNames` VARCHAR(50) NOT NULL,
+  `lastNames` VARCHAR(50) NOT NULL,
+  `age` INT NOT NULL,
+  `gender` VARCHAR(1) NULL,
+  `placeBirth` VARCHAR(50) NULL,
+  `dateBirth` DATE NOT NULL,
+  `civilStatusId` INT NOT NULL,
+  `createDate` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `updateDate` TIMESTAMP NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_legalRepresentative_person1_idx` (`personId` ASC),
+  INDEX `fk_legalRepresentative_documents_person_type1_idx` (`documentsPersonTypeId` ASC),
+  INDEX `fk_legal_representative_civil_status1_idx` (`civilStatusId` ASC),
+  CONSTRAINT `fk_legalRepresentative_person1`
+    FOREIGN KEY (`personId`)
+    REFERENCES `alodigaWallet`.`person` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_legalRepresentative_documents_person_type1`
+    FOREIGN KEY (`documentsPersonTypeId`)
+    REFERENCES `alodigaWallet`.`documents_person_type` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_legal_representative_civil_status1`
+    FOREIGN KEY (`civilStatusId`)
+    REFERENCES `alodigaWallet`.`civil_status` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+ALTER TABLE `alodigaWallet`.`legal_person`
+ADD COLUMN `legalRepresentativeId` BIGINT NULL;
+ALTER TABLE `alodigaWallet`.`legal_person`
+ADD CONSTRAINT `fk_legal_person_legal_representative1`
+ FOREIGN KEY (`legalRepresentativeId`)
+ REFERENCES `alodigaWallet`.`legal_representative` (`id`)
+ ON DELETE NO ACTION
+ ON UPDATE NO ACTION;
+
+
+
+
 
 
 
---
+++ src/com/alodiga/wallet/common/model/PersonHasAddress.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.alodiga.wallet.common.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author jose
+ */
+@Entity
+@Table(name = "person_has_address")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "PersonHasAddress.findAll", query = "SELECT p FROM PersonHasAddress p")
+    , @NamedQuery(name = "PersonHasAddress.findById", query = "SELECT p FROM PersonHasAddress p WHERE p.id = :id")
+    , @NamedQuery(name = "PersonHasAddress.findByCreateDate", query = "SELECT p FROM PersonHasAddress p WHERE p.createDate = :createDate")
+    , @NamedQuery(name = "PersonHasAddress.findByUpdateDate", query = "SELECT p FROM PersonHasAddress p WHERE p.updateDate = :updateDate")})
+public class PersonHasAddress implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "createDate")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createDate;
+    @Column(name = "updateDate")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updateDate;
+    @JoinColumn(name = "addressId", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Address addressId;
+    @JoinColumn(name = "personId", referencedColumnName = "id")
+    @OneToOne(optional = false)
+    private Person personId;
+
+    public PersonHasAddress() {
+    }
+
+    public PersonHasAddress(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    public Address getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(Address addressId) {
+        this.addressId = addressId;
+    }
+
+    public Person getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(Person personId) {
+        this.personId = personId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof PersonHasAddress)) {
+            return false;
+        }
+        PersonHasAddress other = (PersonHasAddress) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.alodiga.wallet.common.model.PersonHasAddress[ id=" + id + " ]";
+    }
+    
+}
