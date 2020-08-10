4f657b45e0ec923589eb630c017909c3ebd52d7b
Jesús Gómez <jedugomvil@hotmail.com>
2020-08-10 09:33:12 -0400

    Agregadas las clases para los tipos de servicio por tipo de negocio

---
+++ src/com/alodiga/wallet/common/model/BusinessType.java
@@ -0,0 +1,129 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.alodiga.wallet.common.model;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+import org.codehaus.jackson.annotate.JsonIgnore;
+
+/**
+ *
+ * @author jose
+ */
+@Entity
+@Table(name = "business_type")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "BusinessType.findAll", query = "SELECT b FROM BusinessType b")
+    , @NamedQuery(name = "BusinessType.findById", query = "SELECT b FROM BusinessType b WHERE b.id = :id")
+    , @NamedQuery(name = "BusinessType.findByDescription", query = "SELECT b FROM BusinessType b WHERE b.description = :description")
+    , @NamedQuery(name = "BusinessType.findByCode", query = "SELECT b FROM BusinessType b WHERE b.code = :code")})
+public class BusinessType implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "description")
+    private String description;
+    @Size(max = 10)
+    @Column(name = "code")
+    private String code;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "businessTypeId")
+    private Collection<BusinessServiceType> businessServiceTypeCollection;
+
+    public BusinessType() {
+    }
+
+    public BusinessType(Integer id) {
+        this.id = id;
+    }
+
+    public BusinessType(Integer id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    @XmlTransient
+    @JsonIgnore
+    public Collection<BusinessServiceType> getBusinessServiceTypeCollection() {
+        return businessServiceTypeCollection;
+    }
+
+    public void setBusinessServiceTypeCollection(Collection<BusinessServiceType> businessServiceTypeCollection) {
+        this.businessServiceTypeCollection = businessServiceTypeCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof BusinessType)) {
+            return false;
+        }
+        BusinessType other = (BusinessType) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.alodiga.wallet.common.model.BusinessType[ id=" + id + " ]";
+    }
+    
+}
