3bf32765f396964bf254d3affacf3ca52433f85a
Yamelis <yamealm@gmail.com>
2020-08-25 17:09:06 -0300

    BusinessEJB

---
+++ src/com/alodiga/wallet/common/ejb/BusinessEJB.java
@@ -0,0 +1,31 @@
+package com.alodiga.wallet.common.ejb;
+
+import java.util.List;
+
+import javax.ejb.Remote;
+
+import com.portal.business.commons.exceptions.EmptyListException;
+import com.portal.business.commons.exceptions.GeneralException;
+import com.portal.business.commons.exceptions.NullParameterException;
+import com.portal.business.commons.models.Business;	
+
+
+@SuppressWarnings(value = {"all"})
+@Remote
+public interface BusinessEJB  {
+
+	    public Business getBusinessById(long id) throws NullParameterException, GeneralException;
+
+	    public Business getBusinessByCode(String code) throws NullParameterException, GeneralException; 
+	    
+	    public Business getBusinessByIdentification(String identification) throws NullParameterException, GeneralException;
+
+	    public Business getBusinessByEmail(String email) throws NullParameterException, GeneralException; 
+
+	    public Business getBusinessByPhone(String phone) throws NullParameterException, GeneralException; 
+
+	    public Business getBusinessByLogin(String login) throws NullParameterException, GeneralException;
+
+	    public List<Business> getAll() throws EmptyListException, GeneralException;
+}
+
