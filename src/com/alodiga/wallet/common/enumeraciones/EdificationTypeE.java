5111cd27ad7d8c0bcdff1f3239ff4a9aa4a90b7b
moisegrat1212 <moisegrat12@gmail.com>
2020-08-31 15:16:55 -0400

    configuracion label

--- src/com/alodiga/wallet/common/enumeraciones/EdificationTypeE.java
+++ src/com/alodiga/wallet/common/enumeraciones/EdificationTypeE.java
@@ -1,4 +1,4 @@
-/*
+    /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -17,7 +17,7 @@ public enum EdificationTypeE {
     RESIDE(4, "Residencia", "RESIDE"),
     CENCOM(5, "Centro Comercial", "CENCOM"),
     CHALET(6, "Chalet", "CHALET"),
-    DUPLEX(6, "Duplex", "DUPLEX");
+    DUPLEX(7, "Duplex", "DUPLEX");
     
     private int id;
     private String edificationTypeDescription;
