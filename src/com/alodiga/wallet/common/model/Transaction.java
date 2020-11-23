45f69b39ad63687457be7b6bbba0b2dfc008cc54
moises <moisegrat12@gmail.com>
2020-11-23 10:33:42 -0400

    modificacion enumeraciones

--- src/com/alodiga/wallet/common/model/Transaction.java
+++ src/com/alodiga/wallet/common/model/Transaction.java
@@ -64,6 +64,8 @@ public class Transaction extends AbstractWalletEntity implements Serializable {
     @Basic(optional = false)    
     @Column(name = "transactionNumber")
     private String transactionNumber;
+    @Column(name = "transactionSequence")
+    private String transactionSequence;
     @Column(name = "totalAmount")
     private Float totalAmount;
     @Column(name = "totalTax")
@@ -314,6 +316,16 @@ public class Transaction extends AbstractWalletEntity implements Serializable {
         this.transactionNumber = transactionNumber;
     }
 
+    public String getTransactionSequence() {
+        return transactionSequence;
+    }
+
+    public void setTransactionSequence(String transactionSequence) {
+        this.transactionSequence = transactionSequence;
+    }
+
+    
+    
     public Float getTotalAmount() {
         return totalAmount;
     }
