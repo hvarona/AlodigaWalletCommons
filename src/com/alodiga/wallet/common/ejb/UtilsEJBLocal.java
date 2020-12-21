3f4fb84c0be8ae6a577478d9f728faa68f803ee8
Jorge Pinto <jorgealepc@gmail.com>
2020-12-21 10:35:41 -0400

    Modificacion del nombre del metodo

--- src/com/alodiga/wallet/common/ejb/UtilsEJBLocal.java
+++ src/com/alodiga/wallet/common/ejb/UtilsEJBLocal.java
@@ -215,7 +215,7 @@ public interface UtilsEJBLocal extends WalletGenericEJB {
     public List<AffiliationRequest> searchAffiliationRequestByParams(EJBRequest request) throws GeneralException, NullParameterException, EmptyListException;
     public List<AffiliationRequest> getAffiliationRequestByRequestByType(EJBRequest request) throws EmptyListException, GeneralException, NullParameterException;
     public List<AffiliationRequest> getAffiliationRequestByUserByType(EJBRequest request) throws GeneralException, NullParameterException, EmptyListException;
-    public List<AffiliationRequest> getAffiliationRequestByUser(EJBRequest request) throws GeneralException, NullParameterException, EmptyListException;
+    public List<AffiliationRequest> getAffiliationRequestByPerson(EJBRequest request) throws GeneralException, NullParameterException, EmptyListException;
     
     //ReviewAffiliationRequest
     public List<ReviewAffiliationRequest> getReviewAffiliationRequest(EJBRequest request) throws EmptyListException, GeneralException, NullParameterException;
